cd9b67aebc8efb1b72c69a8c29484f5130b6087b
Anand Shenoy <anandshenoy@Anands-MacBook-Pro.local>
2017-09-04 03:31:36 -0700

    installing and configuring firebase

--- index.js
+++ index.js
@@ -5,6 +5,20 @@ app.get('/', function (req, res) {
   res.send('Sample Node App is running!')
 })
 var port = 8080;
+
+console.log("Initialize DB Service in the form of Firebase");
+
+var firebase = require("firebase");
+var config = {
+    apiKey: "AIzaSyDy_tuhMnoda5AhcrlP--5iOV86yrNLy0Q",
+    authDomain: "dcidenw-firebase.firebaseapp.com",
+    databaseURL: "https://dcidenw-firebase.firebaseio.com",
+    projectId: "dcidenw-firebase",
+    storageBucket: "dcidenw-firebase.appspot.com",
+    messagingSenderId: "923363221041"
+  };
+firebase.initializeApp(config);
+
 app.listen(port, function () {
   console.log('Example app listening on port '+port)
 })
